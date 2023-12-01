719b537a4e5985fd11e3889363cd7ddb02857964
Qnyyn <2261748711@qq.com>
2023-12-01 20:22:52 +0800

    rebaseModify
    commit
    
    forkFirstCommit
    
    commit
    
    secondForkCommit
    
    commit2
    
    thirdCommit

---
+++ testFork/fork.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+void func(){
+    std::cout<<"这里是commit测试"<<std::endl;
+}
+
+void func2(){
+    std::cout<<"这里是second改动"<<std::endl;
+}
+
+void func3(){
+    std::cout<<"这里是third改动"<<std::endl;
+}
+
+int main(){
+    func();
+
+
+    return 0;
+}
